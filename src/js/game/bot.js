a3ad1539632060833eea55cfbd3f7736bcbaf3f0
Shannon <smsearle41@gmail.com>
2024-08-26 13:45:27 +1000

    abstracted code into a function

--- src/js/game/bot.js
+++ src/js/game/bot.js
@@ -12,34 +12,29 @@ import { MetaMinerBuilding } from "./buildings/miner";
 
 export class ShapezBot {
     constructor(root) {
-        this.root = root; // The game instance, which you need to interact with
+        this.root = root;
         this.init();
     }
 
     init() {
         console.log("Bot initialized");
-        // Add initialization logic here
         // Place the belt
-        const belt = gMetaBuildingRegistry.findByClass(MetaBeltBuilding).createEntity({
+        this.place_building(MetaBeltBuilding, 3, 4);
+        // place extractor
+        this.place_building(MetaMinerBuilding, 3, 5);
+    }
+
+    /*places a building function of the specified type at the position (x,y)*/
+    place_building(buildingType, x, y) {
+        const building = gMetaBuildingRegistry.findByClass(buildingType).createEntity({
             root: this.root,
-            origin: new Vector(3, 4),
+            origin: new Vector(x, y),
             rotation: 0,
             originalRotation: 0,
             rotationVariant: 0,
             variant: defaultBuildingVariant,
         });
-        const extract = gMetaBuildingRegistry.findByClass(MetaMinerBuilding).createEntity({
-            root: this.root,
-            origin: new Vector(3, 5),
-            rotation: 0,
-            originalRotation: 0,
-            rotationVariant: 0,
-            variant: defaultBuildingVariant,
-        });
-        this.root.map.placeStaticEntity(belt);
-        this.root.entityMgr.registerEntity(belt);
-        this.root.map.placeStaticEntity(extract);
-        this.root.entityMgr.registerEntity(extract);
-        this.root.camera.center = new Vector(-5, 2).multiplyScalar(globalConfig.tileSize);
+        this.root.map.placeStaticEntity(building);
+        this.root.entityMgr.registerEntity(building);
     }
 }
