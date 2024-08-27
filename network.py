e28217bb659b0d5089e4beae0f08bf35c115e86f
Rhys Tyne <tynerhys@gmail.com>
2024-08-27 11:24:46 +1000

    basic framework for nn model

--- network.py
+++ network.py
@@ -1,38 +1,57 @@
 ### file containing network framework
+## Rhys Tyne
+
+import os
+
+os.environ["KERAS_BACKEND"] = "tensorflow"
 
 import tensorflow as tf
 import keras
+from keras import layers
 import numpy as np
 import matplotlib.pyplot as plt
 import copy
+import gym
+from gymnasium.wrappers import AtariPreprocessing, FrameStack
+from simpleGame import Game, Belt
 
-optimiser = keras.optimizers.Adam(learning_rate=0.001)
+# get all resource locations for a specified resource
+def get_resource_locations(state, resource):
+    resource_locations = []
+    for i in range(len(state)):
+        for j in range(len(state[0])):
+            if state[i][j] == resource:
+                resource_locations.append((i, j))
+    return resource_locations
 
-# list of possible inputs
-# smaller the better, as stated above
-#    reduce amount buildings which can be placed
-input_space = ["B", "E"] # for now only allowing - belt (B) and extractor (E)
+# find the nearest instance of a given resource to the hub (Manhattan distance)
+def nearest_resource(hub_location, resource_locations):
+    if len(resource_locations) == 0:
+        return None # no resources on the map
+    nearest = (0, 0)
+    distance = 100000 # make large so shorter distance is guaranteed
+    for resource in resource_locations:
+        # does not account for preexisting belts
+        dist = abs(hub_location[0] - resource[0]) + abs(hub_location[1] - resource[1])
+        if dist < distance:
+            nearest = resource
+    return resource
 
-# list of possible actions
-# technically the action space is a function of:
-#   the amount of empty blocks in the desginated space (E)
-#   the amount of possible inputs + rotations (I)
-#   therefore, size(A) = I * E -- which could grow very quickly. model go slow
+# find a path to the hub avoiding pre-existing belts
+def path_to_hub(state, hub_location, tile):
+    return None
 
-# to minimise this size we can:
-#   1. reduce possible inputs
-#       - only allow a selection of buildings, most simple at first only allow belts
-#   2. reduce positions we allow the model to place stuff
-#       - only allow model to place stuff next to pre-existing structures
-# would shape of this be a 2d array or maybe dictionary:
-#   possible squares as keys? adding an element would normally increase
-#   action space by 3, remove 1 cell which has been added to
-action_space = []
 
 # some heuristic function to determine how "good" a state is
 # this approach is more in line with a traditional q-learning model
 # where we as humans define the heuristic.
 # ideally we want to model to do this itself
+# with q-learning we just some initial states
+
+# shapes on screen is good
+# shapes that match goal shape is better
+# belts connecting between resource - hub is good
+# dont know if this will be needed
 def heuristic(state, goal):
     # state is some array of the board
     #   may be a condensed/zoomed in version of the board
@@ -40,104 +59,200 @@ def heuristic(state, goal):
     # goal is the current goal (eg. 30x squares)
     return 0
 
-# like described above, get free spaces which are next to a resource/building
-def get_available_spaces(state):
-    # assume state is 2d array
-    # assume "empty" cell is signified by 0
-    spaces = set()
-    occupied = []
-    cols = len(state)
-    rows = len(state[0])
-    # add all full cells to a list first
-    for i in range(cols):
-        for j in range(rows):
-            if state[i][j] != 0:
-                occupied.append((i, j))
-    # add unoccupied neighbor cells to spaces
-    for i in range(cols):
-        for j in range(rows):
-            if state[i][j] != 0:
-                # add adjacent cells, used set no dont have to worry about duplicates
-                if (i+1, j) not in occupied:
-                    spaces.add((i+1, j))
-                if (i-1, j) not in occupied:
-                    spaces.add((i-1, j))
-                if (i, j+1) not in occupied:
-                    spaces.add((i, j+1))
-                if (i, j-1) not in occupied:
-                    spaces.add((i, j-1))
-    # convert spaces to list and return
-    return list(spaces)
+# game specific information
+action_space = [1, 2, 3]
+#              ["delete", "belt", "extractor", ...]
+#num_actions = len(action_space) # number of actions available to the model
 
-# very crude way of getting state after some in action in some pos
-def get_next_state(state, pos, action):
-    i = pos[0]
-    j = pos[1]
-    new_state = copy.deepcopy(state)
-    new_state[i][j] = action
-    return new_state
-
-# is this useful????
-def create_action_space(input_space, empty_neighbours):
-    actions, spaces = np.meshgrid(input_space, empty_neighbours)
-    return actions, spaces
+seed = 42
+env = gym.make("BreakoutNoFrameskip-v4")  # , render_mode="human")
+# Environment preprocessing
+env = AtariPreprocessing(env)
+# Stack four frames
+env = FrameStack(env, 4)
+env.seed(seed)
+num_actions = 4
 
 
-# test for get spaces
-X = np.zeros((5,5))
-X[1][1] = 2
-empty_neighbours = get_available_spaces(X)
-actions, spaces = create_action_space(input_space, empty_neighbours)
-
-# classic Q-learning model -- no machine learning
-# is this Q-learning I forget exactly what that is...
-class qLearn:
-    def __init__(self, initial_state, inputs):
-        self.state = initial_state
-        self.inputs = inputs
-
-    # update the state
-    def update_state(self, new_state):
-        self.state = new_state
-
-    # find the best move given the current state
-    def next_best_move(self):
-        available_spaces = get_available_spaces(self.state)
-        current_best_score = -10000 # current best move
-        best_move = None
-        best_move_pos = None
-        for space in available_spaces:
-            for action in self.inputs:
-                next_state = get_next_state(self.state, space, action)
-                score = heuristic(next_state)
-                if score > current_best_score:
-                    current_best_score = score
-                    best_move = action
-                    best_move_pos = space
-        return best_move_pos, best_move
-
-    # run model num_steps amount of times
-    def run(self, num_steps):
-        for step in range(num_steps):
-            pos, action = self.next_best_move()
-            next_state = get_next_state(self.state, pos, action)
-            self.update_state(next_state)
-
-# Neural Network class
-class Network:
-    def __init__(self):
-        pass
-
-    def train():
-        """
-        unsupervised training
-            - would require very good goal/loss function
-
-        supervised training:
-            - would require training data, how can we get that?
-        """
-        pass
+# hyperparameters
+gamma = 0.99  # Discount factor for past rewards
+epsilon = 1.0  # Epsilon greedy parameter
+epsilon_min = 0.1  # Minimum epsilon greedy parameter
+epsilon_max = 1.0  # Maximum epsilon greedy parameter
+epsilon_interval = (
+    epsilon_max - epsilon_min
+)  # Rate at which to reduce chance of random action being taken
+batch_size = 32  # Size of batch taken from replay buffer
+max_steps_per_episode = 10000
+max_episodes = 10  # Limit training episodes, will run until solved if smaller than 1
 
 
+# example from "https://keras.io/examples/rl/deep_q_network_breakout/"
+def create_q_model():
+    # Network defined by the Deepmind paper
+    return keras.Sequential(
+        [
+            layers.Lambda(
+                lambda tensor: keras.ops.transpose(tensor, [0, 2, 3, 1]),
+                output_shape=(84, 84, 4),
+                input_shape=(4, 84, 84),
+            ),
+            # Convolutions on the frames on the screen
+            layers.Conv2D(32, 8, strides=4, activation="relu", input_shape=(4, 84, 84)),
+            layers.Conv2D(64, 4, strides=2, activation="relu"),
+            layers.Conv2D(64, 3, strides=1, activation="relu"),
+            layers.Flatten(),
+            layers.Dense(512, activation="relu"),
+            layers.Dense(num_actions, activation="linear"),
+        ]
+    )
 
+# The first model makes the predictions for Q-values which are used to
+# make a action.
+model = create_q_model()
+# Build a target model for the prediction of future rewards.
+# The weights of a target model get updated every 10000 steps thus when the
+# loss between the Q-values is calculated the target Q-value is stable.
+model_target = create_q_model()
 
+optimiser = keras.optimizers.Adam(learning_rate=0.0001)
+
+# Experience replay buffers
+action_history = []
+state_history = []
+state_next_history = []
+rewards_history = []
+done_history = []
+episode_reward_history = []
+running_reward = 0
+episode_count = 0
+frame_count = 0
+# Number of frames to take random action and observe output
+epsilon_random_frames = 50000
+# Number of frames for exploration
+epsilon_greedy_frames = 1000000.0
+# Maximum replay length
+# Note: The Deepmind paper suggests 1000000 however this causes memory issues
+max_memory_length = 100000
+# Train the model after 4 actions
+update_after_actions = 4
+# How often to update the target network
+update_target_network = 10000
+# Using huber loss for stability
+loss_function = keras.losses.Huber()
+
+while True:
+    observation, _ = env.reset()
+    state = np.array(observation)
+    episode_reward = 0
+
+    for timestep in range(1, max_steps_per_episode):
+        frame_count += 1
+
+        # Use epsilon-greedy for exploration
+        if frame_count < epsilon_random_frames or epsilon > np.random.rand(1)[0]:
+            # Take random action
+            action = np.random.choice(num_actions)
+        else:
+            # Predict action Q-values
+            # From environment state
+            state_tensor = keras.ops.convert_to_tensor(state)
+            state_tensor = keras.ops.expand_dims(state_tensor, 0)
+            action_probs = model(state_tensor, training=False)
+            # Take best action
+            action = keras.ops.argmax(action_probs[0]).numpy()
+
+        # Decay probability of taking random action
+        epsilon -= epsilon_interval / epsilon_greedy_frames
+        epsilon = max(epsilon, epsilon_min)
+
+        # Apply the sampled action in our environment
+        state_next, reward, done, _, _ = env.step(action)
+        state_next = np.array(state_next)
+
+        episode_reward += reward
+
+        # Save actions and states in replay buffer
+        action_history.append(action)
+        state_history.append(state)
+        state_next_history.append(state_next)
+        done_history.append(done)
+        rewards_history.append(reward)
+        state = state_next
+
+        # Update every fourth frame and once batch size is over 32
+        if frame_count % update_after_actions == 0 and len(done_history) > batch_size:
+            # Get indices of samples for replay buffers
+            indices = np.random.choice(range(len(done_history)), size=batch_size)
+
+            # Using list comprehension to sample from replay buffer
+            state_sample = np.array([state_history[i] for i in indices])
+            state_next_sample = np.array([state_next_history[i] for i in indices])
+            rewards_sample = [rewards_history[i] for i in indices]
+            action_sample = [action_history[i] for i in indices]
+            done_sample = keras.ops.convert_to_tensor(
+                [float(done_history[i]) for i in indices]
+            )
+
+            # Build the updated Q-values for the sampled future states
+            # Use the target model for stability
+            future_rewards = model_target.predict(state_next_sample)
+            # Q value = reward + discount factor * expected future reward
+            updated_q_values = rewards_sample + gamma * keras.ops.amax(
+                future_rewards, axis=1
+            )
+
+            # If final frame set the last value to -1
+            updated_q_values = updated_q_values * (1 - done_sample) - done_sample
+
+            # Create a mask so we only calculate loss on the updated Q-values
+            masks = keras.ops.one_hot(action_sample, num_actions)
+
+            with tf.GradientTape() as tape:
+                # Train the model on the states and updated Q-values
+                q_values = model(state_sample)
+
+                # Apply the masks to the Q-values to get the Q-value for action taken
+                q_action = keras.ops.sum(keras.ops.multiply(q_values, masks), axis=1)
+                # Calculate loss between new Q-value and old Q-value
+                loss = loss_function(updated_q_values, q_action)
+
+            # Backpropagation
+            grads = tape.gradient(loss, model.trainable_variables)
+            optimiser.apply_gradients(zip(grads, model.trainable_variables))
+
+        if frame_count % update_target_network == 0:
+            # update the the target network with new weights
+            model_target.set_weights(model.get_weights())
+            # Log details
+            template = "running reward: {:.2f} at episode {}, frame count {}"
+            print(template.format(running_reward, episode_count, frame_count))
+
+        # Limit the state and reward history
+        if len(rewards_history) > max_memory_length:
+            del rewards_history[:1]
+            del state_history[:1]
+            del state_next_history[:1]
+            del action_history[:1]
+            del done_history[:1]
+
+        if done:
+            break
+
+    # Update running reward to check condition for solving
+    episode_reward_history.append(episode_reward)
+    if len(episode_reward_history) > 100:
+        del episode_reward_history[:1]
+    running_reward = np.mean(episode_reward_history)
+
+    episode_count += 1
+
+    if running_reward > 40:  # Condition to consider the task solved
+        print("Solved at episode {}!".format(episode_count))
+        break
+
+    if (
+        max_episodes > 0 and episode_count >= max_episodes
+    ):  # Maximum number of episodes reached
+        print("Stopped at episode {}!".format(episode_count))
+        break
