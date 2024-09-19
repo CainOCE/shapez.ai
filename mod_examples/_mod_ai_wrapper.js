83ac132aeec43e6274e38a526046fe73285458a2
bray44542231 <s4749595@student.uq.edu.au>
2024-09-20 08:45:08 +1000

    new reload game function, modified data being transfered over

--- mod_examples/_mod_ai_wrapper.js
+++ mod_examples/_mod_ai_wrapper.js
@@ -13,11 +13,40 @@ const METADATA = {
 //     const response = await makeCall(...);
 //     placeBuildings(root, response);
 // }
+// need the list of goals
+// need a way to send the static required amount i dont know if we can because the way we have it at the moment we have it we only send through it once
+// List of shit
+// CuCuCuCu
+// ----CuCu
+// RuRuRuRu
+// RuRU----
+// Cu----Cu
+// Cu------
+// CrCrCrCr
+// RbRb----
+// CpCpCpCp
+// ScScScSc
+// CgScScCg
+// CbCbCbRb:CwCwCwCw
+// RpRpRpRp:CwCwCwCw
+// --Cg----:--Cr----
+// SrSrSrSr:CyCyCyCy
+// SrSrSrSr:CyCyCyCy:SwSwSwSw
+// CbRbRbCb:CwCwCwCw:WbWbWbWb
+// Sg----Sg:CgCgCgCg:--CyCy--
+// CpRpCp--:SwSwSwSw
+// RuCw--Cw:----Ru--
+// CrCwCrCw:CwCrCwCr:CrCwCrCw:CwCrCwCr
+// Cg----Cr:Cw----Cw:Sy------:Cy----Cy
+// CcSyCcSy:SyCcSyCc:CcSyCcSy
+// CcRcCcRc:RwCwRwCw:Sr--Sw--:CyCyCyCy
+// Rg--Rg--:CwRwCwRw:--Rg--Rg
+// CbCuCbCu:Sr------:--CrSrCr:CwCwCwCw
 
 class Mod extends shapez.Mod {
     init() {
         console.log("Shapez.ai Module Initialized");
-        console.log("root:", this.root);
+        //console.log("root:", this.root);
 
         // Sandbox Mode
         this.modInterface.replaceMethod(shapez.Blueprint, "getCost", function () {
@@ -27,6 +56,11 @@ class Mod extends shapez.Mod {
             return true;
         });
 
+        // save current or quit current game then make a new one wont work as we need the mod laughcher ot stay running ?? or not or do we just need app.py running, can do this way??
+        function newGame(root) {
+            // quits games
+            root.gameState.goBackToMenu();
+        }
 
         /**
          * Simplifies the inbuilt tryPlaceBuilding Method
@@ -37,12 +71,10 @@ class Mod extends shapez.Mod {
          * @returns {Entity}
          */
         function tryPlaceSimpleBuilding(root, building, x, y) {
-            console.log("root: ", root)
+            // console.log("root: ", root);
             return root.logic.tryPlaceBuilding({
                 origin: new shapez.Vector(x, y),
-                building: shapez.gMetaBuildingRegistry.findByClass(
-                    building
-                ),
+                building: shapez.gMetaBuildingRegistry.findByClass(building),
                 originalRotation: 0,
                 rotation: 0,
                 variant: "default",
@@ -50,16 +82,11 @@ class Mod extends shapez.Mod {
             });
         }
 
-
         /* Simplifies the notification system. */
         function simpleNotification(root, msg) {
             // Display a message when called
-            root.hud.signals.notification.dispatch(
-                msg,
-                shapez.enumNotificationType.info
-            );
+            root.hud.signals.notification.dispatch(msg, shapez.enumNotificationType.info);
         }
-
 
         // Register Custom keybinding
         this.modInterface.registerIngameKeybinding({
@@ -72,14 +99,15 @@ class Mod extends shapez.Mod {
             handler: root => {
                 // TEST Ryan: Place belt and extractor
                 // simpleNotification(root, "Ryan's Placement Test")
-                // var u = tryPlaceSimpleBuilding(root, shapez.MetaBeltBuilding, 3, 4)
+                // var u = tryPlaceSimpleBuilding(root, shapez.MetaMinerBuilding, -16, -10);
                 // var v = tryPlaceSimpleBuilding(root, shapez.MetaMinerBuilding, 3, 5)
 
-                transform_data(root)
+                transform_data(root);
+                //newGame(root);
+
                 return shapez.STOP_PROPAGATION;
             },
         });
-
 
         /**
          * Transforms the current gameState to a form readable by our model.
@@ -89,36 +117,89 @@ class Mod extends shapez.Mod {
          */
         function transform_data(root) {
             var gameState = root.gameState;
-            if (gameState == null) { return; }  // Guard Clause
-            simpleNotification(root, "Gathering gameState...")
+            if (gameState == null) {
+                return;
+            } // Guard Clause
+            simpleNotification(root, "Gathering gameState...");
 
             // TODO:  Can this process/strategy be simplified?
-            function extractRelevantDataEntity(entity) {
-                return {
-                    components: entity.components,
-                    uid: entity.uid,
-                };
+            function extractRelevantColumnsEntity(entities) {
+                let entitiesList = [];
+                for (let i = 0; i < entities.length; i++) {
+                    var direction = null;
+                    let entityList = [];
+                    let entity = entities[i]["components"];
+
+                    // get position of entity
+                    let xTile = entity["StaticMapEntity"]["origin"]["x"];
+                    let yTile = entity["StaticMapEntity"]["origin"]["y"];
+
+                    // 0 is north, 90 is east, 180 is south, 270 is west
+                    let rotation = entity["StaticMapEntity"]["rotation"];
+
+                    // gets the type of the enitity
+                    let type;
+                    if (entity["ItemProcessor"] != null) {
+                        type = entity["ItemProcessor"]["type"];
+                    } else if (entity["UndergroundBelt"] != null) {
+                        type = entity["UndergroundBelt"]["mode"];
+                    } else if (entity["Miner"] != null) {
+                        type = "Miner";
+                    } else if (entity["Belt"]) {
+                        type = "Belt";
+                        direction = entity["Belt"]["direction"];
+                    } else {
+                        throw new Error("no registered Entity");
+                    }
+
+                    //pushs components to an array
+                    entityList.push(type);
+                    entityList.push(xTile);
+                    entityList.push(yTile);
+                    entityList.push(rotation);
+                    entityList.push(direction);
+                    entitiesList.push(entityList);
+                }
+                return entitiesList;
             }
 
-            function extractRelevantDataGoal(goal) {
-                return {
-                    definiton: goal.definition,
-                    required: goal.required,
-                };
+            function staticGoal(stored, goal, required) {
+                // need a way to not count the ones already in once a level is complete
+                for (let key in stored) {
+                    if (key == goal) {
+                        required -= stored[key];
+                    }
+                }
+                return required;
             }
 
-            function extractRelevantDataMap(map) {
-                return {
-                    contents: map.contents,
-                    lowerLayer: map.lowerLayer,
-                    patches: map.patches,
-                    tileSpaceRectangle: map.tileSpaceRectangle,
-                    tileX: map.tileX,
-                    tileY: map.tileY,
-                    worldSpaceRectangle: map.worldSpaceRectangle,
-                    x: map.x,
-                    y: map.y,
-                };
+            // calculates each resource position and puts it in a dictionary
+            function calculateResourcePosition(map, dict) {
+                // Goes through lower layer array and gets a resource
+                for (let i = 0; i < 16; ++i) {
+                    for (let j = 0; j < 16; ++j) {
+                        // select a tile within the chunk
+                        var resource = map["lowerLayer"][i][j];
+
+                        // stores type and position of resource
+                        if (resource instanceof shapez.ColorItem) {
+                            let temp = [];
+                            let filtered_colour = resource["color"];
+                            temp.push(filtered_colour);
+                            temp.push(i + map["tileX"]);
+                            temp.push(j + map["tileY"] - 1);
+                            dict.push(temp);
+                        } else if (resource instanceof shapez.ShapeItem) {
+                            let temp = [];
+                            let filtered_shape = resource["definition"]["cachedHash"];
+                            temp.push(filtered_shape);
+                            temp.push(i + map["tileX"]);
+                            temp.push(j + map["tileY"]);
+                            dict.push(temp);
+                        }
+                    }
+                }
+                return dict;
             }
 
             // TODO: Chunks or map?
@@ -130,43 +211,50 @@ class Mod extends shapez.Mod {
 
             // Process State
             if (gameState["key"] == "InGameState") {
-                var newGameState = [];
+                // Contains three arrays 1. Entities 2. hub goal 3. resources
+                var filteredGameState = [];
 
-                // 1. Extract Entities
-                simpleNotification(root, "Extracting Entities...")
+                // 1. Extract Entities [Type, X (Tile), Y (Tile), Rotation, direction]
+                simpleNotification(root, "Extracting Entities...");
                 var entity_list = [];
                 var entities = gameState["core"]["root"]["entityMgr"]["entities"];
-                for (let i = 0; i < entities.length; i++) {
-                    let new_entity = extractRelevantDataEntity(entities[i]);
-                    entity_list.push(new_entity);
-                }
-                newGameState.push(entity_list);
 
-                // 2. Extract Goals
-                simpleNotification(root, "Extracting Goals...")
+                entity_list = extractRelevantColumnsEntity(entities);
+                filteredGameState.push(entity_list);
+
+                // 2. Extract Goals [Goal ID of shape, static amount]
+                simpleNotification(root, "Extracting Goals...");
+                var stored = gameState["core"]["root"]["hubGoals"]["storedShapes"];
+                var level = gameState["core"]["root"]["hubGoals"]["level"];
                 var goal = gameState["core"]["root"]["hubGoals"]["currentGoal"];
-                let transfferedGoal = extractRelevantDataGoal(goal);
-                newGameState.push(transfferedGoal);
+                let transfferedGoal = [];
+                var statics = staticGoal(stored, goal["definition"]["cachedHash"], goal["required"]);
+                
+                transfferedGoal.push(goal["definition"]["cachedHash"]);
+                transfferedGoal.push(statics);
+                // transfferedGoal.push(goal["required"]);
+                filteredGameState.push(transfferedGoal);
 
-                // 3. Extract Map
-                simpleNotification(root, "Extracting Map Features...")
+                // 3. Extract Map [type, X (Tile), Y (Tile)]
+                simpleNotification(root, "Extracting Map Features...");
                 var mapChunkList = [];
                 var map = gameState["core"]["root"]["map"]["chunksById"];
+                let dict = [];
                 for (const [key, value] of map) {
-                    let dict = {};
-                    dict[key] = extractRelevantDataMap(value);
-                    mapChunkList.push(dict);
+                    dict = calculateResourcePosition(value, dict);
                 }
-                newGameState.push(mapChunkList);
+                mapChunkList.push(dict);
 
+                filteredGameState.push(mapChunkList);
+
+                console.log(filteredGameState);
                 // Send test package to Python Backend
-                simpleNotification(root, "Querying AI Model...")
-                var test = sendGameStateToPythongameState(newGameState);
-                simpleNotification(root, "AI Model Query Complete")
+                simpleNotification(root, "Querying AI Model...");
+                var test = sendGameStateToPythongameState(filteredGameState);
                 console.log(test);
+                simpleNotification(root, "AI Model Query Complete");
             }
         }
-
 
         /**
          * Receives a signal from the python backend.
@@ -175,7 +263,7 @@ class Mod extends shapez.Mod {
          * @returns {type} None
          */
         function receive_signal() {
-            return
+            return;
         }
 
         /**
