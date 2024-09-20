f157a36c7efb7d79ec03f42bb83c41041412bf91
Cain Bruhn-Tanzer <CainBT@outlook.com>
2024-09-20 15:54:50 +1000

    Updated Test Structure

---
+++ src_ai/tests/architect_training.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 20, 2024 at 13:35:38
+
+@author: Cain Bruhn-Tanzer
+"""
+
+if __name__ == "__main__":
+    print("Please call this module as a dependency or import.")
---
+++ src_ai/tests/overseer_training.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 20, 2024 at 13:35:38
+
+@author: Cain Bruhn-Tanzer
+"""
+
+# Lay a path from G to O
+simple_layout_5x5 = [
+    ['_', '_', '_', '_', '_'],
+    ['_', '_', '_', 'O', '_'],
+    ['_', '_', '_', '_', '_'],
+    ['_', 'G', '_', '_', '_'],
+    ['_', '_', '_', '_', '_'],
+]
+
+if __name__ == "__main__":
+    print("Please call this module as a dependency or import.")
