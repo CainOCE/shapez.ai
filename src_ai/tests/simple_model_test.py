0e167b53269d87f287fec387c2993e75dcb77351
Cain Bruhn-Tanzer <CainBT@outlook.com>
2024-09-20 16:10:39 +1000

    Removed Redundant Files

--- src_ai/tests/simple_model_test.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Sep 20, 2024 at 13:35:38
-
-@author: Cain Bruhn-Tanzer
-"""
-
-if __name__ == "__main__":
-    print("Please call this module as a dependency or import.")
