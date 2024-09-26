ec11ef00dde9e5a565d831e6856f078a7b658f06
Cain Bruhn-Tanzer <CainBT@outlook.com>
2024-09-26 11:57:07 +1000

    Update to pytest for testing

--- src_ai/tests/test_architect_training.py
+++ src_ai/tests/test_architect_training.py
@@ -5,5 +5,11 @@ Created on Fri Sep 20, 2024 at 13:35:38
 @author: Cain Bruhn-Tanzer
 """
 
+
+def test_case1():
+    """ Dummy A """
+    assert f"Hello {1}" == "Hello 1", "Passed"
+
+
 if __name__ == "__main__":
     print("Please call this module as a dependency or import.")
