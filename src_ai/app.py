4a67ea33abcbaa371a2ab89784b39bee30d9db53
Cain Bruhn-Tanzer <CainBT@outlook.com>
2024-09-27 14:56:32 +1000

    Vastly simplified the Signals.py and app.py

--- src_ai/app.py
+++ src_ai/app.py
@@ -4,30 +4,42 @@ Created on Tue Aug 13, 2024 at 09:55:35
 
 @authors: Cain Bruhn-Tanzer, Rhys Tyne
 """
+# System Imports
 import sys
 import os
-from game import Game, Chunk
-from model import Overseer, Architect, RhysArchitect
-from signals import ListenServer
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+from game import GameState
+from model import Overseer, Architect
 
 # TODO:  Correct module imports for subfolder structure src_ai/app.py
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 
+class ShapezAI(Flask):
+    """ App built atop FLASK to provide a REST API for the python backend. """
+    def __init__(self):
+        super().__init__(__name__)
+        CORS(self)
+        self._routing()
+
+        self.game = GameState()
+        self.overseer = Overseer()
+        self.architect = Architect()
+
+    def _routing(self):
+        """Sets up the routes for the Flask app."""
+
+        @self.route('/query_model', methods=['POST'])
+        def on_query():
+            """ Handles incoming queries sent by the game instance. """
+            self.game.update(request.json)
+            return jsonify(self.architect.query(request.json))
+
+
 if __name__ == "__main__":
     print("Running the Shapez.ai module...")
-
-    # Start our AI Model Classes
-    overseer = Overseer()
-    architect = Architect()
-    for model in [overseer, architect]:
-        STATE = 'Active' if model.is_alive() else 'Inactive'
-        print(f"{model.get_name()} is {STATE}")
-
-    # Test the model merges (Rhys Model -> Architect)
-    # architect.train()
-
-    # Test our Send and Receive Functions
-    sigs = ListenServer()
-    sigs.start()
-    sigs.send()
+    app = ShapezAI()
+    app.run(debug=False)
